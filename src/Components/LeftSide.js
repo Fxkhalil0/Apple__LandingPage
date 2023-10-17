4fdd310010cad48adfeb74fc5fc95dbfd45a6cb3
Fxkhalil0 <fxkhalil0@gmail.com>
2023-10-17 04:21:29 +0300

    LeftSide

---
+++ src/Components/LeftSide.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Apple from '../assets/apple (1).svg'
+
+
+
+
+
+
+
+
+function LeftSide() {
+    return (
+    <div className="leftside__section">
+        <img src={Apple} alt="" />
+    </div>
+    );
+}
+
+export default LeftSide;
