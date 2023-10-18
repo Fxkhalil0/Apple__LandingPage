50504dfd4271d135bd525ce7d30501d1c27b82af
Fxkhalil0 <fxkhalil0@gmail.com>
2023-10-18 05:02:21 +0300

    Add components

--- src/Components/LeftSide.js
+++ src/Components/LeftSide.js
@@ -1,6 +1,5 @@
 import React from "react";
-import Apple from '../assets/apple (1).svg'
-
+import NavLogo from '../assets/navlogo.png'
 
 
 
@@ -10,9 +9,33 @@ import Apple from '../assets/apple (1).svg'
 
 function LeftSide() {
     return (
-    <div className="leftside__section">
-        <img src={Apple} alt="" />
-    </div>
+        <div className="leftside__section">
+            <div className="container">
+                <nav>
+                    <img src={NavLogo} alt="" />
+                    <ul>
+                        <li>Mac</li>
+                        <li>iPad</li>
+                        <li>iPhone</li>
+                        <li>Watch</li>
+                        <li>TV</li>
+                        <li>Music</li>
+                        <li>Support</li>
+                        <li><i class="fa-solid fa-magnifying-glass" style={{ color: '#F6F5F1', fontSize: '23px' }}></i></li>
+                        <li><i class='bx bx-shopping-bag' style={{ color: '#F6F5F1', fontSize: '27px' }}  ></i></li>
+                    </ul>
+                </nav>
+                <div className="page__product">
+                    <div className="page__content">
+                        <h1>AirPods Max</h1>
+                       <p>Introducing AirPods Max - a perfect balance of exhilarating high-fidelity audio and the effortless magic of AirPods. The ultimate personal listening experience is here.</p> 
+                    </div>
+                    <div className="page__image">
+
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
